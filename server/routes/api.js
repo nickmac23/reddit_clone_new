4962188d7cdf0962fcfb685b1fce23f194812591
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-05-10 23:31:34 -0600

    configed api call to return object with comments array

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js')
+var postarray = []
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('authors')
   .leftOuterJoin('posts', 'posts.author_fk', 'authors.author_id')
-  // .leftOuterJoin('comments', 'comments.author_fk', 'authors.author_id')
-  .then( function (authors) {
-    console.log(authors);
-    res.json(authors)
+  .then( function (posts) {
+    postarray = posts
+    return postarray
+  }).then( function (postarray){
+    knex('authors')
+    .innerJoin('comments', 'comments.author_fk', 'authors.author_id')
+    .then( function (comments) {
+      for (var i = 0; i < postarray.length; i++) {
+        postarray[i].comments = [];
+        for (var j = 0; j < comments.length; j++) {
+          if (postarray[i].post_id === comments[j].post_fk) {
+            postarray[i].comments.push({
+                                        comment: comments[j].comment,
+                                        date:    comments[j].date,
+                                        name: comments[j].name,
+                                        author_id: comments[j].author_id
+                                      })
+          }
+        }
+      }
+      res.json(postarray)
+    })
   })
 });
 
